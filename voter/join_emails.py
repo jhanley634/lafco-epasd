3c1d7a76c670ca010a73830c7d7aa120548f6dd8
John Hanley <jhanley708-git@s6.sector6.net>
2024-07-20 18:08:25 -0700

    JOIN and then write out ..._with_phone_email.csv

--- voter/join_emails.py
+++ voter/join_emails.py
@@ -31,11 +31,23 @@ def get_protest() -> pd.DataFrame:
 
 def report() -> None:
     voter = get_voter()
+    voter["addr"] = voter.ResidenceAddress.str.rstrip().str.upper()
+    voter = voter.rename(columns={"PhoneNumber": "phone"})
+    voter = voter.rename(columns={"EmailAddress": "email"})
+    voter["email"] = voter.email.str.lower()
+    voter = voter[["addr", "phone", "email"]]
+
     protest = get_protest()
-    protest["phone"] = voter.PhoneNumber
-    protest["email"] = voter.EmailAddress
-    print(protest)
+    protest = protest.rename(columns={"epa_address": "addr"})
+    joined = protest.merge(voter, on="addr", how="left")
+
+    fname = "2024-05-21_qry_EPASD_APNs_Landowner_Protests_with_phone_email.csv"
+    fspec = Path("/tmp") / fname
+    joined.to_csv(fspec, index=False)
+    print(f"Wrote {len(joined)} rows to {fspec}")
+    assert (836259, "3a3dcd0a") == fingerprint(fspec), fingerprint(fspec)
 
 
 if __name__ == "__main__":
-    typer.run(report)
+    with pd.option_context("display.max_columns", None):
+        typer.run(report)
